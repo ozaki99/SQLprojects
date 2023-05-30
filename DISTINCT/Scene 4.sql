e570eede4c04b5e8851fefaf668a12fea72b6c31
ozaki99 <m.ozturk1251@gmail.com>
2023-05-31 02:25:46 +0300

    SQL Project 4 created by ozaki99 / 31 May 2023 | 02.25 AM

---
+++ DISTINCT/Scene 4.sql
@@ -0,0 +1,4 @@
+--country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+
+SELECT COUNT(*) FROM country
+WHERE country LIKE '_____';
---
+++ DISTINCT/Scene 5.sql
@@ -0,0 +1,4 @@
+--city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+
+SELECT COUNT(*) FROM city
+WHERE city ILIKE '%R';
